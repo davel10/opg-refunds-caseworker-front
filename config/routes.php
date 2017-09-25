e5f6c80f95a8f5a2c89e71e4cb59296e9fd55414
David Winchurch <david.winchurch@iterativeit.com>
2017-09-25 14:39:04 +0100

    Call through to API to set no Sirius/Meris POAs found

--- config/routes.php
+++ config/routes.php
@@ -41,3 +41,7 @@ $app->get('/set-password', App\Action\PasswordSetNewAction::class, 'password.set
 $app->get('/download', App\Action\DownloadAction::class, 'download');
 $app->get('/process-new-claim', App\Action\ProcessNewClaimAction::class, 'process.new.claim');
 $app->route('/claim/{id:\d+}', App\Action\ClaimAction::class, ['GET', 'POST'], 'claim');
+$app->get('/claim/{id:\d+}/poa/sirius/none-found', App\Action\Poa\PoaSiriusNoneFoundAction::class, 'claim.poa.sirius.none.found');
+$app->get('/claim/{id:\d+}/poa/sirius/cancel-none-found', App\Action\Poa\PoaSiriusCancelNoneFoundAction::class, 'claim.poa.sirius.cancel.none.found');
+$app->get('/claim/{id:\d+}/poa/meris/none-found', App\Action\Poa\PoaMerisNoneFoundAction::class, 'claim.poa.meris.none.found');
+$app->get('/claim/{id:\d+}/poa/meris/cancel-none-found', App\Action\Poa\PoaMerisCancelNoneFoundAction::class, 'claim.poa.meris.cancel.none.found');
